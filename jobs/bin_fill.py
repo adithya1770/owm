452d49cdc83934bce10a4e4311292b55c04387c2
adithya1770 <adithyaps929@gmail.com>
2025-07-14 12:20:47 +0530

    logical error #2

--- jobs/bin_fill.py
+++ jobs/bin_fill.py
@@ -6,16 +6,16 @@ def call_bin():
     bins = supabase.table("bins").select("bin_id", "fill_level", "status").execute().data
 
     id_array = [int(bin_["bin_id"]) for bin_ in bins]
-
     bin_id = random.choice(id_array)
 
-    current_bin = supabase.table("bins").select("fill_level").eq("bin_id", bin_id).execute().data[0]
+    current_bin = supabase.table("bins").select("fill_level", "status").eq("bin_id", bin_id).execute().data[0]
     current_fill = current_bin["fill_level"]
+    status = current_bin["status"]
 
-    if bins["status"] == "not filled":
+    if status == "not filled":
         supabase.table("bins").update({"fill_level": current_fill + 5}).eq("bin_id", bin_id).execute()
+        print("Bin Updated!")
     else:
         print("Bin is already filled.")
-    print("Bin Updated!")
 
     time.sleep(10)
