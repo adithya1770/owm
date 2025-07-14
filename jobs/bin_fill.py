65549fdea29ab3c5c84666b5ae72d9b977a70c20
adithya1770 <adithyaps929@gmail.com>
2025-07-14 12:10:47 +0530

    resolved logical error

--- jobs/bin_fill.py
+++ jobs/bin_fill.py
@@ -3,7 +3,7 @@ from client import supabase
 import time
 
 def call_bin():
-    bins = supabase.table("bins").select("bin_id, fill_level").execute().data
+    bins = supabase.table("bins").select("bin_id", "fill_level", "status").execute().data
 
     id_array = [int(bin_["bin_id"]) for bin_ in bins]
 
@@ -12,7 +12,10 @@ def call_bin():
     current_bin = supabase.table("bins").select("fill_level").eq("bin_id", bin_id).execute().data[0]
     current_fill = current_bin["fill_level"]
 
-    supabase.table("bins").update({"fill_level": current_fill + 5}).eq("bin_id", bin_id).execute()
+    if bins["status"] == "not filled":
+        supabase.table("bins").update({"fill_level": current_fill + 5}).eq("bin_id", bin_id).execute()
+    else:
+        print("Bin is already filled.")
     print("Bin Updated!")
 
     time.sleep(10)
